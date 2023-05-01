7afd8a0427f9d5d848c515caf0cb82357edf757b
nolines <cemre.by@gmail.com>
2023-05-01 23:23:20 +0200

    database replaced with h2 for test purpose

--- src/test/kotlin/com/urlShortenerApp/urlShortenerApp/repository/UrlRepositoryTest.kt
+++ src/test/kotlin/com/urlShortenerApp/urlShortenerApp/repository/UrlRepositoryTest.kt
@@ -3,15 +3,10 @@ package com.urlShortenerApp.urlShortenerApp.repository
 import com.urlShortenerApp.urlShortenerApp.model.Url
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
-import org.springframework.test.context.junit4.SpringRunner
 
-@RunWith(SpringRunner::class)
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UrlRepositoryTest(
     @Autowired
     private var urlRepository: UrlRepository
