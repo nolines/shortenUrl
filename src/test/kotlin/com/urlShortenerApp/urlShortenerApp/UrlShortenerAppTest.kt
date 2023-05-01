98ee992bfea09c75b98a30ce26ccc7e7ceb01f62
nolines <cemre.by@gmail.com>
2023-05-01 21:06:06 +0200

    add application context test template

---
+++ src/test/kotlin/com/urlShortenerApp/urlShortenerApp/UrlShortenerAppTest.kt
@@ -0,0 +1,11 @@
+package com.urlShortenerApp.urlShortenerApp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UrlShortenerAppTest {
+    @Test
+    fun contextLoads() {
+    }
+}
